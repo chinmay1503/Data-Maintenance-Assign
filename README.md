1ec46af226a369ba3890c8d95c89a3c281081766
Chinmay Tawde <ctawde@pdx.edu>
2022-05-25 21:31:38 -0700

    Code Submission for Data Maintenance Assignment

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Data-Maintenance-Assign
+
+This repo contains code written for the Data Maintenance Assignment for the course CS-510: Data Engineering taught by Professor Bruce Irvin at Portland State University The activity details and answers are present in the document DataEng S22: Data Maintenance In-class Assignment.pdf
---
+++ producer/archive_compress.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+#
+# Copyright 2020 Confluent Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# =============================================================================
+#
+# Consume messages from Confluent Cloud
+# Using Confluent Python Client for Apache Kafka
+#
+# =============================================================================
+
+from confluent_kafka import Consumer
+import json
+import ccloud_lib
+from datetime import date
+import pandas as pd
+import time
+import psycopg2
+import psycopg2.extras
+import argparse
+import re
+import csv
+from dateutil import parser
+import logging
+import os
+import gzip
+from google.cloud import storage
+
+def create_json(jsonData):
+    filename = '/home/rsa-key-20220518/maintenance-assign/Compressed_Data_Engg' + str(date.today()) + '.json'
+    json_obj = json.dumps(jsonData)
+    encoded = json_obj.encode('utf-8')
+    compressed = gzip.compress(encoded)
+    with gzip.open(filename, "w") as outfile:
+         outfile.write(compressed)
+    jsonData.clear()
+    return filename
+
+def upload_to_bucket(filename, bucket_name = 'data-maintenance-assign'): 
+    bucket = storage.Client().bucket(bucket_name)
+    blob = bucket.blob('data/Compressed_Data_Engg' + str(date.today()) + '.json')
+    blob.upload_from_filename(filename)
+
+if __name__ == '__main__':
+
+    # Read arguments and configurations and initialize
+    args = ccloud_lib.parse_args()
+    config_file = args.config_file
+    topic = args.topic
+    conf = ccloud_lib.read_ccloud_config(config_file)
+
+    # Create Consumer instance
+    # 'auto.offset.reset=earliest' to start reading from the beginning of the
+    #   topic if no committed offsets exist
+    consumer_conf = ccloud_lib.pop_schema_registry_params_from_config(conf)
+    consumer_conf['group.id'] = 'archive_group_2'
+    consumer_conf['auto.offset.reset'] = 'earliest'
+    consumer = Consumer(consumer_conf)
+
+    # Subscribe to topic
+    consumer.subscribe([topic])
+    
+    jsonData = []
+    try:
+        while True:
+            msg = consumer.poll(1.0)
+            if msg is None:
+                # No message available within timeout.
+                # Initial message consumption may take up to
+                # `session.timeout.ms` for the consumer group to
+                # rebalance and start consuming
+                print("Waiting for message or event/error in poll()")
+                if len(jsonData) > 0:
+                   filename = create_json(jsonData)
+                   upload_to_bucket(filename)
+            elif msg.error():
+                print('error: {}'.format(msg.error()))
+            else:
+                # Check for Kafka message
+                record_key = msg.key()
+                record_value = msg.value()
+                data = json.loads(record_value)
+                jsonData.append(data)
+                print("Consumed record with key {} and value {}, \
+                      and data is {}"
+                      .format(record_key, record_value, data))
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Leave group and commit final offsets
+        consumer.close()
+
